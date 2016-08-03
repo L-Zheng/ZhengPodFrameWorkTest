498301e913a16f0906d32b0315ca8a7d2c56e1cb
李保征 <418736259@qq.com>
2016-08-03 13:14:11 +0800

    创建frameWork工程,创建podspec文件,版本设置0.0.1

---
+++ Products/ZhengPodFrameWorkTestExample.framework/Headers/ZhengPodFrameWorkTestExample.h
@@ -0,0 +1,20 @@
+//
+//  ZhengPodFrameWorkTestExample.h
+//  ZhengPodFrameWorkTestExample
+//
+//  Created by 李保征 on 16/8/3.
+//  Copyright © 2016年 李保征. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ZhengPodFrameWorkTestExample/DevelopmentTool.h>
+
+//! Project version number for ZhengPodFrameWorkTestExample.
+FOUNDATION_EXPORT double ZhengPodFrameWorkTestExampleVersionNumber;
+
+//! Project version string for ZhengPodFrameWorkTestExample.
+FOUNDATION_EXPORT const unsigned char ZhengPodFrameWorkTestExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZhengPodFrameWorkTestExample/PublicHeader.h>
+
+
